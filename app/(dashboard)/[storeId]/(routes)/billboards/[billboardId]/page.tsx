e3d97861dbc9e206237f161091ee46008b185c48
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-03-18 17:57:53 -0300

    ecommerce10

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,25 +1,26 @@
-import prismadb from '@/lib/prismadb'
-import React from 'react'
-import BillBoardForm from './components/billboard-form'
+import prismadb from '@/lib/prismadb';
+import React from 'react';
+import BillBoardForm from './components/billboard-form';
 
 interface BillboardPageProps {
-    params: { billboardId: string }
+    params: { billboardId: string };
 }
 
 const BillboardPage = async ({ params }: BillboardPageProps) => {
+    // Busca os dados do banco de dados
     const billboard = await prismadb.billboard.findUnique({
         where: {
-            id: params.billboardId
-        }
-    })
+            id: params.billboardId,
+        },
+    });
 
     return (
-        <div className='flex-col'>
-            <div className='flex-1 space-y-4 p-8 pt-6'>
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
                 <BillBoardForm initialData={billboard} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default BillboardPage
+export default BillboardPage;
