3bbd29c8c90e530d6b2835a11a9cca96dcaff986
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-03-18 16:10:52 -0300

    ecommerce4

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -2,21 +2,23 @@ import prismadb from '@/lib/prismadb'
 import React from 'react'
 import BillBoardForm from './components/billboard-form'
 
+// BillboardPage is no longer async itself, but we will fetch data in a separate async function.
 const BillboardPage = async ({
-    params
+  params,
 }: {
-    params: { billboardId: string }
+  params: { billboardId: string }
 }) => {
+  // Fetch the billboard data
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  })
 
-    const billboard = await prismadb.billboard.findUnique({
-        where: {
-            id: params.billboardId
-        }
-    })
-
+  // Return the JSX
   return (
-    <div className='flex-col'>
-      <div className='flex-1 space-y-4 p-8 pt-6'>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
         <BillBoardForm initialData={billboard} />
       </div>
     </div>
