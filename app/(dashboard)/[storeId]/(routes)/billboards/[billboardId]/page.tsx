6855e9a9b0316f1d702b6994e15a6672204d2b53
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-03-18 16:28:25 -0300

    ecommerce7

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,25 +1,35 @@
 import prismadb from '@/lib/prismadb'
 import React from 'react'
 import BillBoardForm from './components/billboard-form'
+import { Billboard } from '@prisma/client'
+import { GetServerSideProps } from 'next'
 
-const BillboardPage = async ({
-  params,
-}: {
-  params: { billboardId: string }
-}) => {
-  const billboard = await prismadb.billboard.findUnique({
-    where: {
-      id: params.billboardId,
-    },
-  })
+type BillboardPageProps = {
+  billboard: Billboard | null  // Aqui você usa o tipo 'Billboard' gerado pelo Prisma
+}
 
+const BillboardPage = ({ billboard }: BillboardPageProps) => {
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
         <BillBoardForm initialData={billboard} />
       </div>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params?.billboardId as string
+    }
+  })
+
+  return {
+    props: {
+      billboard
+    }
+  }
 }
 
 export default BillboardPage
